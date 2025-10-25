e3df2afcc4f1c4635c08a4783591c64337adf61b
cesar pagliacci <pagliaccicesar@gmail.com>
2025-10-25 16:55:30 -0300

    se agrego nuevas fotos

--- main.js
+++ main.js
@@ -91,7 +91,21 @@ sr.reveal('.contact-text', {interval: 200});
 sr.reveal('.contact-input', {delay: 400});
 sr.reveal('.contact-button', {delay: 600});
 
+//// galeria que hacemos
 
+const gM = document.querySelector(".galeria-modal");
+const iGM = document.querySelector(".galeria-modal img");
+
+function fecharGaleria(){
+    gM.style.visibility = "hidden";
+    iGM.style.transform = "scale(0)";
+}
+
+function abrirGaleria(src){
+    gM.style.visibility = "visible";
+    iGM.style.transform = "scale(1)";
+    iGM.src = src
+}
 
 //// correo
 document.getElementById("contactForm").addEventListener("submit", function(e){
@@ -113,6 +127,107 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
         console.error(error);
     });
 });
+
+///////carr segundo carrousel
+class Slider {
+    constructor(slider) {
+        this.slider = slider;
+        this.display = slider.querySelector(".image-display");
+        this.navButtons = Array.from(slider.querySelectorAll(".nav-button"));
+        this.prevButton = slider.querySelector(".prev-button");
+        this.nextButton = slider.querySelector(".next-button");
+        this.sliderNavigation = slider.querySelector(".slider-navigation");
+        this.currentSlideIndex = 0;
+        this.preloadedImages = {};
+
+        this.initialize();
+    }
+
+    initialize() {
+        this.setupSlider();
+        this.preloadImages();
+        this.eventListeners();
+    }
+
+    setupSlider() {
+        this.showSlide(this.currentSlideIndex);
+    }
+
+    showSlide(index) {
+        this.currentSlideIndex = index;
+        const navButtonImg = this.navButtons[
+            this.currentSlideIndex
+        ].querySelector("img");
+        if (navButtonImg) {
+            const imgClone = navButtonImg.cloneNode();
+            this.display.replaceChildren(imgClone);
+        }
+        this.updateNavButtons();
+    }
+
+    updateNavButtons() {
+        this.navButtons.forEach((button, buttonIndex) => {
+            const isSelected = buttonIndex === this.currentSlideIndex;
+            button.setAttribute("aria-selected", isSelected);
+            if (isSelected) button.focus();
+        });
+    }
+
+    preloadImages() {
+        this.navButtons.forEach((button) => {
+            const imgElement = button.querySelector("img");
+            if (imgElement) {
+                const imgSrc = imgElement.src;
+                if (!this.preloadedImages[imgSrc]) {
+                    this.preloadedImages[imgSrc] = new Image();
+                    this.preloadedImages[imgSrc].src = imgSrc;
+                }
+            }
+        });
+    }
+
+    eventListeners() {
+        document.addEventListener("keydown", (event) => {
+            this.handleAction(event.key);
+        });
+
+        this.sliderNavigation.addEventListener("click", (event) => {
+            const targetButton = event.target.closest(".nav-button");
+            const index = targetButton
+                ? this.navButtons.indexOf(targetButton)
+                : -1;
+            if (index !== -1) {
+                this.showSlide(index);
+            }
+        });
+
+        this.prevButton.addEventListener("click", () =>
+            this.handleAction("prev")
+        );
+        this.nextButton.addEventListener("click", () =>
+            this.handleAction("next")
+        );
+    }
+
+    handleAction(action) {
+        if (action === "Home") {
+            this.currentSlideIndex = 0;
+        } else if (action === "End") {
+            this.currentSlideIndex = this.navButtons.length - 1;
+        } else if (action === "ArrowRight" || action === "next") {
+            this.currentSlideIndex =
+                (this.currentSlideIndex + 1) % this.navButtons.length;
+        } else if (action === "ArrowLeft" || action === "prev") {
+            this.currentSlideIndex =
+                (this.currentSlideIndex - 1 + this.navButtons.length) %
+                this.navButtons.length;
+        }
+
+        this.showSlide(this.currentSlideIndex);
+    }
+}
+
+const ImageSlider = new Slider(document.querySelector(".image-slider"));
 
 
 
