5de094755652157c06950eaf2cf74a9115805225
cesar pagliacci <pagliaccicesar@gmail.com>
2025-11-25 15:51:15 -0300

    se agrego textos

--- main.js
+++ main.js
@@ -130,6 +130,30 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
     });
 });
 
+////  porque elegirnos
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const btn = item.querySelector(".faq-question");
+
+  btn.addEventListener("click", () => {
+    // close other items
+    faqItems.forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+        i.querySelector(".icon").textContent = "+";
+      }
+    });
+
+    // toggle current item
+    item.classList.toggle("active");
+
+    const icon = item.querySelector(".icon");
+    icon.textContent = item.classList.contains("active") ? "–" : "+";
+  });
+});
+
+
 
 
 
